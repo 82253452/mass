48139db9e275bb24a34d29915565f292e73b2b72
yp <82253452@qq.com>
2018-11-27 18:58:51 +0800

    init

--- src/main/java/com/f4w/weapp/NotifyController.java
+++ src/main/java/com/f4w/weapp/NotifyController.java
@@ -82,7 +82,7 @@ public class NotifyController {
         } catch (WxErrorException e) {
             e.printStackTrace();
         }
-        response.sendRedirect("/web/index.html#/busi/busiApp");
+        response.sendRedirect("/index.html#/busi/busiApp");
     }
 
     @RequestMapping("message")
