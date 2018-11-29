02effc0c036c44f3cc4b20b338926c5fab843e51
yp <82253452@qq.com>
2018-11-29 14:43:27 +0800

    init

---
+++ src/main/resources/admin/src/busi/components/Dictionary.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="height: 100%;width:100%;background: #fff;text-align: center">
+    <h1 style="margin-top:40%;">字典</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    }
+  },
+
+  data() {
+    return {
+      temp: ''
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
