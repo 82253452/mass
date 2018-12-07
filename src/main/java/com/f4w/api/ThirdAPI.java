d0f1141fcead1d38630bd82ca9faf1048512a12f
yp <82253452@qq.com>
2018-12-07 21:38:08 +0800

    init

--- src/main/java/com/f4w/api/ThirdAPI.java
+++ src/main/java/com/f4w/api/ThirdAPI.java
@@ -1,11 +1,13 @@
 package com.f4w.api;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.f4w.entity.juhe.DictionaryResult;
 import com.f4w.entity.news.NewsResult;
 import com.f4w.utils.HttpUtils;
 import com.f4w.utils.R;
 import com.github.kevinsawicki.http.HttpRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -53,15 +55,146 @@ public class ThirdAPI {
     }
 
     @GetMapping("/nameRate")
-    public R laohuangli(String fName, String name)  {
+    public R laohuangli(String fName, String name) {
         JSONObject s = nameRate(fName, name);
         return R.renderSuccess("data", s);
     }
 
     @GetMapping("/nameMathing")
-    public R nameMath(String fName, String name)  {
+    public R nameMath(String fName, String name) {
         JSONObject s = nameMathing(fName, name);
         return R.renderSuccess("data", s);
+    }
+
+    @GetMapping("/CooKArticleDetail")
+    public R CooKArticleDetail(String id) {
+        if (StringUtils.isBlank(id)) {
+            return R.error();
+        }
+        HttpRequest request = HttpRequest.get("https://m.meishij.net/huodong/zt_detail.php?id=" + id);
+        String s = request.body();
+        Document doc = Jsoup.parse(s);
+        Elements topImg = doc.select(".zt2018_main .topimgw img");
+        Elements title = doc.select(".zt2018_main .zttitlew");
+        Elements content = doc.select("html body .zt2018_main .ztcontentw div > *");
+        Elements contentF = new Elements();
+        Elements contentD = new Elements();
+        AtomicInteger f = new AtomicInteger();
+        content.forEach(c -> {
+            if ("div".equalsIgnoreCase(c.tagName())) {
+                f.set(1);
+                return;
+            }
+            if (!"p".equalsIgnoreCase(c.tagName())) {
+                return;
+            }
+            if (f.get() == 0) {
+                contentF.add(c);
+            } else {
+                contentD.add(c);
+            }
+        });
+        Elements newsList = doc.select("html body .zt2018_main .ztcontentw .nzt_cpitem_style1 a");
+        JSONArray jsonArray = new JSONArray();
+        newsList.forEach(t -> {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("url", t.attr("href"));
+            jsonObject.put("image", t.select(".cpimgw img").attr("src"));
+            jsonObject.put("title", t.select(".cpn").text());
+            jsonArray.add(jsonObject);
+        });
+        JSONObject result = new JSONObject();
+        result.put("title", title.text());
+        result.put("topImg", topImg.attr("src"));
+        result.put("content", content.toString());
+        result.put("contentF", contentF.toString());
+        result.put("contentD", contentD.toString());
+        result.put("newsList", jsonArray);
+        return R.renderSuccess("data", result);
+    }
+
+    @GetMapping("/CooKVideo")
+    public R CooKVideo(String id) {
+        if (StringUtils.isBlank(id)) {
+            return R.error();
+        }
+        HttpRequest request = HttpRequest.get("https://apph5.meishi.cc/article/article_detail.php?id=" + id);
+        String s = request.body();
+        Document doc = Jsoup.parse(s);
+        String vSrc = doc.select(".topimgw2 video").first().attr("src");
+        JSONArray contentImg = new JSONArray();
+        String title = doc.selectFirst(".main .title").text();
+        Elements imagesE = doc.select(".main .artical_content p img");
+        imagesE.forEach(e -> {
+            contentImg.add(e.attr("src"));
+        });
+        JSONObject result = new JSONObject();
+        result.put("vSrc", vSrc);
+        result.put("contentImg", contentImg);
+        result.put("title", title);
+        return R.renderSuccess("data", result);
+    }
+
+    @GetMapping("/CooKArticleNews")
+    public R CooKArticleNews(String id) {
+        if (StringUtils.isBlank(id)) {
+            return R.error();
+        }
+        HttpRequest request = HttpRequest.get("https://www.meishij.net/news.php?id=" + id);
+        String s = request.body();
+        Document doc = Jsoup.parse(s);
+        String img = doc.select(".main_w .main .cp_header .cp_headerimg_w img").get(0).attr("src");
+        String name = doc.select(".main_w .main .cp_header .cp_main_info_w .title a").text();
+        String title = doc.select(".main_w .main .cp_body .measure h2").first().text();
+        Elements gongxiaoE = doc.select(".main_w .main .cp_header .cp_main_info_w .yj_tags a");
+        JSONArray gongxiao = new JSONArray();
+        gongxiaoE.forEach(e -> {
+            gongxiao.add(e.text());
+        });
+        Elements zle = doc.select(".main_w .main .cp_body .materials_box .zl li");
+        Elements fle = doc.select(".main_w .main .cp_body .materials_box .fuliao li");
+        JSONArray yongliaoZ = new JSONArray();
+        JSONArray yongliaoF = new JSONArray();
+        zle.forEach(e -> {
+            JSONObject jsonObject = new JSONObject();
+            String zImg = e.selectFirst("img").attr("src");
+            String zName = e.selectFirst("h4 a").text();
+            String zNum = e.selectFirst("h4 span").text();
+            jsonObject.put("zImg", zImg);
+            jsonObject.put("zName", zName);
+            jsonObject.put("zNum", zNum);
+            yongliaoZ.add(jsonObject);
+        });
+        fle.forEach(e -> {
+            JSONObject jsonObject = new JSONObject();
+            String zName = e.selectFirst("h4 a").text();
+            String zNum = e.selectFirst("span").text();
+            jsonObject.put("zName", zName);
+            jsonObject.put("zNum", zNum);
+            yongliaoF.add(jsonObject);
+        });
+        Elements stempE = doc.select(".main_w .main .cp_body .measure .editnew .content");
+        JSONArray stemp = new JSONArray();
+        stempE.forEach(e -> {
+            JSONObject jsonObject = new JSONObject();
+            final String[] text = {""};
+            Elements textE = e.select(".c");
+            textE.forEach(e2 -> {
+                text[0] += e2.text() + "\n";
+            });
+            String sImg = e.select("img").attr("src");
+            jsonObject.put("text", text[0]);
+            jsonObject.put("img", sImg);
+            stemp.add(jsonObject);
+        });
+        JSONObject result = new JSONObject();
+        result.put("img", img);
+        result.put("name", name);
+        result.put("title", title);
+        result.put("yongliaoZ", yongliaoZ);
+        result.put("yongliaoF", yongliaoF);
+        result.put("stemp", stemp);
+        return R.renderSuccess("data", result);
     }
 
     private JSONObject nameMathing(String fName, String sName) {
@@ -83,9 +216,9 @@ public class ThirdAPI {
         result.put("text", text);
         result.put("qianshi", qianshi);
         result.put("qianshiText", qianshiText);
-        System.out.println(result.toJSONString());
         return result;
     }
+
     private JSONObject nameRate(String fName, String sName) {
         Map<String, String> data = new HashMap<>();
         data.put("xingx", fName);
@@ -135,6 +268,20 @@ public class ThirdAPI {
         });
         result.put("xiangjie", xiangjie[0]);
         return result;
+    }
+
+    public static void main(String[] args) {
+        HttpRequest request = HttpRequest.get("https://apph5.meishi.cc/article/article_detail.php?id=57976");
+        String s = request.body();
+        Document doc = Jsoup.parse(s);
+        String vSrc = doc.select(".topimgw2 video").first().attr("src");
+        JSONArray contentImg = new JSONArray();
+        String title = doc.selectFirst(".main .title").text();
+        Elements imagesE = doc.select(".main .artical_content p img");
+        imagesE.forEach(e -> {
+            contentImg.add(e.attr("src"));
+        });
+        System.out.println(title);
     }
 
 
