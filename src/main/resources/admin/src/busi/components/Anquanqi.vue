9a3ad5f561d172a87e607c4957ec0ac3236ff594
yp <82253452@qq.com>
2018-12-19 12:12:09 +0800

    init

---
+++ src/main/resources/admin/src/busi/components/Anquanqi.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main"/>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: {}
+    }
+  },
+
+  data() {
+    return {
+      temp: ''
+    }
+  },
+  computed: {}
+}
+</script>
+<style>
+  .main {
+    height: 100%;
+    width: 100%;
+    background: #fff;
+    text-align: center;
+    border: 1px dashed #bfcbd9;
+    background: url("../../assets/images/anquanqi.jpg")  no-repeat !important;
+    background-size:100% !important;
+  }
+</style>
