e05ad38d977a32072ead27ac3df2147ee4285840
yp <82253452@qq.com>
2019-01-10 14:55:47 +0800

    init

--- src/main/java/com/f4w/controller/BusiAppController.java
+++ src/main/java/com/f4w/controller/BusiAppController.java
@@ -97,7 +97,8 @@ public class BusiAppController {
                 try {
                     WxMpMaterialNews.WxMpMaterialNewsArticle news = new WxMpMaterialNews.WxMpMaterialNewsArticle();
                     news.setTitle(e.getTitle());
-                    File file = new File(UUID.randomUUID().toString() + ".png");
+                    File file = File.createTempFile(null,"png");
+//                    File file = new File(UUID.randomUUID().toString() + ".png");
                     String thumbnail = e.getThumbnail();
                     if (StringUtils.isNotBlank(thumbnail)) {
                         URL url = new URL(thumbnail);
