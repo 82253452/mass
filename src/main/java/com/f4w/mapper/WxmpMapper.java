7b375698a940b4c7ab7604baf64b1d4e71084723
yp <82253452@qq.com>
2019-02-12 22:18:58 +0800

    init

--- src/main/java/com/f4w/mapper/WxmpMapper.java
+++ src/main/java/com/f4w/mapper/WxmpMapper.java
@@ -50,9 +50,7 @@ public interface WxmpMapper extends BaseMapper<Wxmp> {
     @Select("select column_id from wxmp group by column_id order by  column_id")
     List<String> findColumns();
 
-    @Delete("DELETE wx " +
-            "FROM wxmp wx " +
-            "WHERE wx.id IN (SELECT max(w.id) FROM wxmp w GROUP BY w.title HAVING count(*) > 1)")
+    @Delete("DELETE wx FROM wxmp wx WHERE wx.id IN (select t.id from (SELECT max(id) as id FROM wxmp  GROUP BY title HAVING count(*) > 1) t)")
     void deleteDuplicates();
 
 }
