8108bdedbb64503463aa5994466e4fde2eca958a
yuping <yuping@zhonglanmedia.com>
2020-09-17 20:45:45 +0800

    demand

--- src/main/java/com/f4w/controller/CarTypeController.java
+++ src/main/java/com/f4w/controller/CarTypeController.java
@@ -2,15 +2,13 @@ package com.f4w.controller;
 
 import com.f4w.dto.req.CarTypeReq;
 import com.f4w.entity.CarType;
+import com.f4w.entity.TransCompany;
 import com.f4w.mapper.CarTypeMapper;
 import com.f4w.service.WechatService;
 import com.f4w.utils.ForeseenException;
 import com.f4w.utils.Result;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -28,6 +26,18 @@ public class CarTypeController {
     public Result<PageInfo<CarType>> list(CarTypeReq req) throws ForeseenException {
         PageInfo<CarType> page = PageInfo.of(carTypeMapper.getList(req));
         return Result.ok(page);
+    }
+
+    @PostMapping
+    public Result add(@RequestBody CarType req) throws ForeseenException {
+        int i = carTypeMapper.insertSelective(req);
+        return Result.ok(i);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody CarType req) throws ForeseenException {
+        int i = carTypeMapper.updateByPrimaryKeySelective(req);
+        return Result.ok(i);
     }
 
 }
