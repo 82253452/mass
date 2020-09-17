64cc832262e7527cd140785450d1653c278a0025
yuping <yuping@zhonglanmedia.com>
2020-09-17 17:59:50 +0800

    demand

--- src/main/java/com/f4w/dto/TransCompanyUserDto.java
+++ src/main/java/com/f4w/dto/TransCompanyUserDto.java
@@ -1,11 +1,18 @@
 package com.f4w.dto;
 
 import com.f4w.entity.SysUser;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 /**
  * @Author: yp
  * @Date: 2020/9/17 16:16
  */
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class TransCompanyUserDto extends SysUser {
     private Integer status;
 }
