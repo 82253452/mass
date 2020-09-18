5f75cb04bda8e6bf609b02c9d9bc4befae9f24e4
yuping <yuping@zhonglanmedia.com>
2020-09-18 17:31:01 +0800

    demand

--- src/main/java/com/f4w/mapper/OrderMapper.java
+++ src/main/java/com/f4w/mapper/OrderMapper.java
@@ -118,7 +118,7 @@ public interface OrderMapper extends BaseMapper<Order> {
             "from `order` o\n" +
             "         left join car_type ct on o.product_id = ct.id\n" +
             "where o.delete = 0\n" +
-            " and o.status = 0\n" +
+            " and o.status = #{status}\n" +
             " and o.receive_user_id = #{userId}\n" +
             "order by o.mtime desc\n" +
             "</script>")
